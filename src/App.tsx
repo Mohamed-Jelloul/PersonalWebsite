fc3a2f7e4de7f40b4f82da48ff270def5991dfd9
Mohamed-Jelloul <simojelloul22@gmail.com>
2022-10-13 11:45:15 -0400

    Added element of randomness

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={"https://simosandboxbucket.s3.us-east-2.amazonaws.com/IMG_0828.jpg"} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+    let [imgUrl, setImageUrl] = useState("https://simosandboxbucket.s3.us-east-2.amazonaws.com/IMG_0828.jpg")
+    let [description, setDescription] = useState("The Best Programmer on Earth")
+    let imgUrls = {
+        0: "https://simosandboxbucket.s3.us-east-2.amazonaws.com/IMG_0828.jpg",
+        1: "https://simosandboxbucket.s3.us-east-2.amazonaws.com/IMG_0906.jpg"
+    }
+
+    const fetchImage = async () => {
+        let index = Math.round(Math.random()%2)
+        console.log(index)
+        // @ts-ignore
+        setImageUrl(imgUrls[index])
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={imgUrl} className="App-logo" alt="logo" />
+                <p>{description}</p>
+                <button onClick={fetchImage}>Randomize Image</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
