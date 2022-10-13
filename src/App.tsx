4a0d744bc3ae7aa970198d2ed8f7ddd8053103ba
Mohamed-Jelloul <simojelloul22@gmail.com>
2022-10-13 12:14:32 -0400

    fixed Simo image bug

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    let [imgUrl, setImageUrl] = useState("https://simosandboxbucket.s3.us-east-2.amazonaws.com/IMG_0828.jpg")
+    let [imgUrl, setImageUrl] = useState("https://simosandboxbucket.s3.us-east-2.amazonaws.com/Moi.jpg")
     let [description, setDescription] = useState("The Best Programmer on Earth")
     let imgUrls = {
         0: "https://simosandboxbucket.s3.us-east-2.amazonaws.com/Moi.jpg",
@@ -13,7 +13,7 @@ function App() {
     }
 
     const fetchImage = async () => {
-        let index = Math.round(Math.random()*3)
+        let index = Math.floor(Math.random()*3)
         console.log(index)
         // @ts-ignore
         setImageUrl(imgUrls[index])
