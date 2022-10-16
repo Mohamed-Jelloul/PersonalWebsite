ec48962ac8615c1ce2abe958ce58070963150880
Mohamed-Jelloul <simojelloul22@gmail.com>
2022-10-16 15:00:56 -0400

    added cookies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React, {useState} from 'react';
+import {CookiesProvider, useCookies} from "react-cookie";
+import {Button, ButtonGroup, TextField} from '@mui/material';
 import './App.css';
 
 function App() {
     let [imgUrl, setImageUrl] = useState("https://simosandboxbucket.s3.us-east-2.amazonaws.com/Moi.jpg")
-    let [description, setDescription] = useState("The Best Programmer on Earth")
+    let [name, setName] = useState('')
+    let [cookie, setCookie] = useCookies(['name'])
 
     function fetchImage() {
         fetch('https://api.mohamedjelloul.com/get_random_image_url')
@@ -15,14 +18,27 @@ function App() {
             .catch((err) => console.log(err.message))
     }
 
+    const handleName = () => {
+        setCookie('name', name, {path: '/', expires: new Date(9999, 10, 10)})
+        console.log(cookie['name'])
+    }
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={imgUrl} className="App-logo" alt="logo" />
-                <p>{description}</p>
-                <button onClick={fetchImage}>Get Random Image</button>
-            </header>
-        </div>
+        <CookiesProvider>
+            <div className="App">
+                <header className="App-header">
+                    <img src={imgUrl} className="App-logo" alt="logo" />
+                    <p>Hello {cookie['name']}!</p>
+                    <TextField id="outlined-basic" label="Enter Name" variant="filled" onChange={(e) => {
+                        setName(e.target.value)
+                    }}/>
+                    <ButtonGroup sx={{"marginTop": 5}} variant="contained" aria-label="outlined primary button group">
+                        <Button onClick={handleName}>Set Name</Button>
+                        <Button onClick={fetchImage}>Random Image</Button>
+                    </ButtonGroup>
+                </header>
+            </div>
+        </CookiesProvider>
     );
 }
 
